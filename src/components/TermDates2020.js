120f7fcc1d4ee93a0adf6472c67a84316ac793ef
raj <rraju12@gmail.com>
2020-08-11 20:05:30 +0100

    Change term dates for year 2020

--- src/components/TermDates2020.js
+++ src/components/TermDates2020.js
@@ -2,65 +2,59 @@ import React from "react"
 const TermDates2020 = () => (
   <>
     <h5 className="blue-text text-lighten-2">
-      Term dates for next academic year (Sept 2019 - July 2020)
+      Term dates for next academic year (Sept 2020 - July 2021)
     </h5>
     <span className="termDates-header">Autumn Term</span>
     <br />
-    <i>9th Sept setting up (No Children in)</i>
+    <i>7th Sept setting up (No Children in)</i>
     <ul className=" list grey-text text-darken-2">
       <li>
-        <i className=" tiny material-icons">child_care</i>Tuesday 10th Sept –Fri
-        18th Oct{" "}
+        <i className=" tiny material-icons">child_care</i>7th Sept – 
+        23rd Oct{" "}
       </li>
       <li>
-        <i className=" tiny material-icons">child_care</i> Mon 21st Oct – Fri
-        25th Oct 1-week half term closed
+        <i className=" tiny material-icons">child_care</i>26th - 30th Oct 1 week half term closed
       </li>
       <li>
-        <i className=" tiny material-icons">child_care</i>Mon 28th Oct –
-        Thursday 19th Dec
+        <i className=" tiny material-icons">child_care</i>2nd Nov –
+        11th Dec
       </li>
       <li>
-        <i className=" tiny material-icons">child_care</i> Friday 20th Dec – Fri
-        3rd Jan 2 and half weeks Christmas closed
+        <i className=" tiny material-icons">child_care</i>14th Dec – 1st Jan  and 3 weeks Christmas holiday
       </li>
     </ul>
     <span className="termDates-header">Spring Term</span>
     <br />
-    <i>Monday 6th Jan setting up (No Children in)</i>
+    <i>Monday 4th Jan setting up (No Children in)</i>
     <ul className=" list grey-text text-darken-2">
       <li>
-        <i className=" tiny material-icons">child_care</i>Tuesday 7 Jan –Fri
-        14th Feb
+        <i className=" tiny material-icons">child_care</i>4th Jan – 
+        12th Feb
       </li>
       <li>
-        <i className=" tiny material-icons">child_care</i>Mon 17th Feb – Fri
-        21st Feb 1-week half term closed
+        <i className=" tiny material-icons">child_care</i>15th Feb – 19th Feb 1 week half term closed
       </li>
       <li>
-        <i className=" tiny material-icons">child_care</i>Mon 24th Feb – 3rd
-        April Fri
+        <i className=" tiny material-icons">child_care</i>22nd Feb – 31st
+        March
       </li>
       <li>
-        <i className=" tiny material-icons">child_care</i>Mon 6th April – Fri
-        17th Apr 2 weeks Easter holidays closed
+        <i className=" tiny material-icons">child_care</i>1st April – 16th Apr 2 weeks Easter holiday closed
       </li>
     </ul>
     <span className="termDates-header">Summer Term</span>
     <br />
-    <i>Monday 20th setting up (No Children in)</i>
+    <i>19th April setting up (No Children in)</i>
     <ul className=" list grey-text text-darken-2">
       <li>
-        <i className=" tiny material-icons">child_care</i>Tues 21st April –Fri
-        22nd May
+        <i className=" tiny material-icons">child_care</i>19th April – 28th May
       </li>
       <li>
-        <i className=" tiny material-icons">child_care</i>Mon 25th May – 29th
-        May 1-week half term closed
+        <i className=" tiny material-icons">child_care</i>31st May – 4th
+       June 1 week half term closed
       </li>
       <li>
-        <i className=" tiny material-icons">child_care</i>Monday 1st June – Fri
-        24th July
+        <i className=" tiny material-icons">child_care</i>7th June – 23rd July
       </li>
       {/* <li>
         <i className=" tiny material-icons">child_care</i>Tues 4 Jun – Fri 19
@@ -77,21 +71,30 @@ const TermDates2020 = () => (
     <ul className=" list grey-text text-darken-2">
       <li>
         <i className=" tiny material-icons">child_care</i>Christmas Party
-        <span> </span>Thursday 19 Dec
+        <span> </span>11th December 2020
       </li>
       <li>
         <i className=" tiny material-icons">child_care</i>Easter Party
-        <span> </span>Friday 3rd April
+        <span> </span>31st March
       </li>
       <li>
         <i className=" tiny material-icons">child_care</i>Bank Holiday
-        <span> </span> Monday 4th May
+        <span> </span>3rd May
+      </li>
+      <li>
+        <i className=" tiny material-icons">child_care</i>Polling day
+        <span> </span>6th May
       </li>
       <li>
         <i className=" tiny material-icons">child_care</i>Leavers Party
-        <span> </span>Friday 24th July
+        <span> </span>23rd July
       </li>
     </ul>
+    <p className="grey-text text-darken-2">
+      <span className="termDates-header">**</span>
+      Only parents, grandparents and siblings are welcome to attend preschool parties
+      <span className="termDates-header">**</span>
+    </p>
   </>
 )
 export { TermDates2020 as default }

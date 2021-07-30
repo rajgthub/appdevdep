eb9ac0885a4848c3b00957acf21dfaad96ae8c49
raj <rraju12@gmail.com>
2021-07-30 22:27:42 +0100

    Updated term dates and details.

--- src/components/Fundraising.js
+++ src/components/Fundraising.js
@@ -1,18 +1,18 @@
-import React from "react"
-import VacancyAlert from './VacancyAlert'
+import React from "react";
+import VacancyAlert from "./VacancyAlert";
 const Fundraising = () => {
   return (
-    <div className='col s12 homeWrapper'>
+    <div className="col s12 homeWrapper">
       <VacancyAlert />
-      <h5 className='blue-text text-lighten-2'>Fundraising</h5>
-      <p className='grey-text text-darken-2'>
-        As a registered charity the preschool does various fundraisers
-        throughout the year and the most popular being 'Peddle for Preschool'.
-        The children enjoy this activity through play & raise money through
-        sponsorship.
+      <h5 className="blue-text text-lighten-2">Fundraising</h5>
+      <p className="grey-text text-darken-2">
+        We are a registered charity preschool and we organise various
+        fundraising events such as Peddle for preschool, World Book Day,
+        Alphabet Collects, Summer Tambola, Brick and Brack sale and Raffles.
+        Those are aimed to involve parents and children to take part in
+        preschool establishment.
       </p>
     </div>
-  )
-}
-
-export default Fundraising
+  );
+};
+export default Fundraising;
---
+++ src/components/TermDates2021.js
@@ -0,0 +1,95 @@
+import React from "react"
+const TermDates2021 = () => (
+  <>
+    <h5 className="blue-text text-lighten-2">
+      Term dates for next academic year (Sept 2021 - July 2022)
+    </h5>
+    <span className="termDates-header">Autumn Term 2021</span>
+    <br />
+    <i>6th Sept setting up (No Children in)</i>
+    <ul className=" list grey-text text-darken-2">
+      <li>
+        <i className=" tiny material-icons">child_care</i>7th Sept –
+        22rd Oct{" "}
+      </li>
+      <li>
+        <i className=" tiny material-icons">child_care</i>25th - 29th Oct 1 week half term closed
+      </li>
+      <li>
+        <i className=" tiny material-icons">child_care</i>1st Nov –
+        10th Dec
+      </li>
+      <li>
+        <i className=" tiny material-icons">child_care</i>13th Dec – 4th Jan  and 3 weeks Christmas holiday
+      </li>
+    </ul>
+    <span className="termDates-header">Spring Term 2022</span>
+    <br />
+    <i>Tuesday 4th Jan setting up (No Children in)</i>
+    <ul className=" list grey-text text-darken-2">
+      <li>
+        <i className=" tiny material-icons">child_care</i>5th Jan –
+        11th Feb
+      </li>
+      <li>
+        <i className=" tiny material-icons">child_care</i>14th Feb – 18th Feb 1 week half term closed
+      </li>
+      <li>
+        <i className=" tiny material-icons">child_care</i>21st Feb – 1st
+        April
+      </li>
+      <li>
+        <i className=" tiny material-icons">child_care</i>4th April – 15th April 2 weeks Easter holiday closed
+      </li>
+    </ul>
+    <span className="termDates-header">Summer Term 2022</span>
+    <br />
+    <i>18th April setting up (No Children in)</i>
+    <ul className=" list grey-text text-darken-2">
+      <li>
+        <i className=" tiny material-icons">child_care</i>19th April – 27th May
+      </li>
+      <li>
+        <i className=" tiny material-icons">child_care</i>30th May – 3rd
+       June 1 week half term closed
+      </li>
+      <li>
+        <i className=" tiny material-icons">child_care</i>6th June – 22nd July
+      </li>
+      {/* <li>
+        <i className=" tiny material-icons">child_care</i>Tues 4 Jun – Fri 19
+        Jul
+      </li> */}
+    </ul>
+    <br />
+    <p className="grey-text text-darken-2">
+      <span className="termDates-header">*</span>
+      All children and parents are invited to the Friday mornings Party Days.
+      All younger siblings and parents only. Sorry no other relations invited.
+    </p>
+    <br />
+    <ul className=" list grey-text text-darken-2">
+      <li>
+        <i className=" tiny material-icons">child_care</i>Christmas Party
+        <span> </span>10th December 2022
+      </li>
+      <li>
+        <i className=" tiny material-icons">child_care</i>Easter Party
+        <span> </span>1st April 2022
+      </li>
+      <li>
+        <i className=" tiny material-icons">child_care</i>Bank Holiday
+        <span> </span>2nd May 2022
+      </li>
+      <li>
+        <i className=" tiny material-icons">child_care</i>Polling day
+        <span> </span>5th May 2022
+      </li>
+      <li>
+        <i className=" tiny material-icons">child_care</i>Leavers Party
+        <span> </span>23rd July 2022
+      </li>
+    </ul>
+  </>
+)
+export { TermDates2021 as default }

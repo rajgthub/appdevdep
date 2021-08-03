269cfe2c6fbc97bb3d91b2b8a607c5a7bafc78ed
raj <rraju12@gmail.com>
2021-08-03 23:02:48 +0100

    Add current intake for vacancy alert.

--- src/components/VacancyAlert.js
+++ src/components/VacancyAlert.js
@@ -4,7 +4,7 @@ export default function VacancyAlert() {
   return (
     <div className="alert_message">
       <h6 className="top_message">
-        We are accepting vacancies.{" "}
+        We are accepting vacancies available for current intake.{" "}
         <Link to="Contactus"> Contact us </Link>today to secure your place!
       </h6>
     </div>

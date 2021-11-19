7020623cdf97c53fce401297f121fd226e5a0ba5
raj <rraju12@gmail.com>
2021-11-19 00:19:10 +0000

    Added childcare message.

--- src/components/VacancyAlert.js
+++ src/components/VacancyAlert.js
@@ -7,6 +7,14 @@ export default function VacancyAlert() {
         We are accepting vacancies for current intake.{" "}
         <Link to="Contactus"> Contact us </Link>today to secure your place!
       </h6>
+      <h5 className="bottom_message">
+        <p className="bottom_top_paragraph">
+          We are spreading our wings to open up 30 hours childcare from Jan 2022.
+        </p>
+        <p>
+          Open day is on 3rd of December from 10 am for families and carers to visit our exciting preschool to see us in action!!
+        </p>
+      </h5>
     </div>
   );
 }
